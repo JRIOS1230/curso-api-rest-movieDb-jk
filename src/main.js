3396e3749f89dcbf980d7c94f259018000328562
Juan Camilo Rios Tabares <juanc.rios@satrack.com>
2024-06-30 23:52:34 -0500

    Configuracion de peliculas para el contenedor de peliculas horizontal

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log('API URL = https://dfdlfkjdf.com/naskjndkjnfd?api_key=' + API_KEY)
+//aca haremos una ciclo el cual va a colocar para que este se guarde en un movie container,para el elemento de scroll horizontal
+//para que estas se agreguen con su respectiva imagen
+
+ async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+  
+    const movies = data.results;
+    movies.forEach(movie => {
+      const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+      
+      const movieContainer = document.createElement('div');
+      movieContainer.classList.add('movie-container');
+  
+      const movieImg = document.createElement('img');
+      movieImg.classList.add('movie-img');
+      movieImg.setAttribute('alt', movie.title);
+      movieImg.setAttribute(
+        'src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+      );
+  
+      movieContainer.appendChild(movieImg);
+      trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+  }
