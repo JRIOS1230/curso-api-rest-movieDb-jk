00023717beeb9d3d5bcc39e493eea17d7634058f
Juan Camilo Rios Tabares <juancamiloriostabares@outlook.com>
2024-07-24 21:25:46 -0500

    Ajuste de archivos faltantes al proyecto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-//aca haremos una ciclo el cual va a colocar para que este se guarde en un movie container,para el elemento de scroll horizontal
-//para que estas se agreguen con su respectiva imagen
-
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
@@ -11,48 +8,46 @@ const api = axios.create({
   },
 });
 
- async function getTrendingMoviesPreview() {
+async function getTrendingMoviesPreview() {
   const { data } = await api('trending/movie/day');
   const movies = data.results;
 
-    movies.forEach(movie => {
-      const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
-      
-      const movieContainer = document.createElement('div');
-      movieContainer.classList.add('movie-container');
-  
-      const movieImg = document.createElement('img');
-      movieImg.classList.add('movie-img');
-      movieImg.setAttribute('alt', movie.title);
-      movieImg.setAttribute(
-        'src', 
-        'https://image.tmdb.org/t/p/w300/' + movie.poster_path
-      );
-      movieContainer.appendChild(movieImg);
-      trendingPreviewMoviesContainer.appendChild(movieContainer);
-    });
-  }
-
-  async function getCategoriesPreview() {
-    const { data } = await api('genre/movie/list');
-    const Categories = data.genres;
+  movies.forEach(movie => {
+    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
     
-    console.log(Categories)
-    Categories.forEach(category => {
-      const trendingPreviewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
-      
-      const categoryContainer = document.createElement('div');
-      categoryContainer.classList.add('category-container');
-  
-      const categoryTitle = document.createElement('h3');
-      categoryTitle.classList.add('category-title');
-      categoryTitle.setAttribute('id', 'id' + category.id);
-      const categoryTitleText = document.createTextNode(category.name)
-      
-      categoryTitle.appendChild(categoryTitleText)
-      categoryContainer.appendChild(categoryTitle);
-      trendingPreviewCategoriesContainer.appendChild(categoryContainer);
-    });
-  }
-  getTrendingMoviesPreview()
-  getCategoriesPreview()
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute(
+      'src',
+      'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+    );
+
+    movieContainer.appendChild(movieImg);
+    trendingPreviewMoviesContainer.appendChild(movieContainer);
+  });
+}
+
+async function getCategegoriesPreview() {
+  const { data } = await api('genre/movie/list');
+  const categories = data.genres;
+
+  categories.forEach(category => {
+    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+    
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id', 'id' + category.id);
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+  });
+}
